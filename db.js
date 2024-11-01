71987e56f08776e75bec73412a8536bde1fed149
ezoooooooooo <ezooahmed39@gmail.com>
2024-11-01 22:41:50 +0200

    Update db code again

--- db.js
+++ db.js
@@ -9,14 +9,7 @@ const connectDB = async () => {
       throw new Error('MONGO_URI is not defined in environment variables');
     }
 
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // Additional recommended options:
-      serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
-      retryWrites: true
-    });
-
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     return conn;
   } catch (err) {
