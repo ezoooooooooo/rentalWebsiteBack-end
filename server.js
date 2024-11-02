20c8ab7ac65b48812067462c07ed5dd6621daa2d
ezoooooooooo <ezooahmed39@gmail.com>
2024-11-02 22:26:56 +0200

    Integrate authRoutes with server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./db");
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
@@ -7,6 +8,8 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Welcome to the Rent Website API");
 });
+
+app.use('/api', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
