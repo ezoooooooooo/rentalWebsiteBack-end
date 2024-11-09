a9db5f63f25024d21e0019e3cd5c34dca2012d85
ezoooooooooo <ezooahmed39@gmail.com>
2024-11-09 22:17:34 +0200

    Add validator for phone & birthdate

--- middleware/validator.middleware.js
+++ middleware/validator.middleware.js
@@ -1,41 +1,45 @@
-
-const validator = require('validator');
+const validator = require("validator");
 
 exports.validateSignup = (req, res, next) => {
-    const { firstName, lastName, email, password, phone, birthDate, address } = req.body;
-    const errors = {};
+  const { firstName, lastName, email, password, phone, birthDate, address } =
+    req.body;
+  const errors = {};
 
-    if (!firstName?.trim()) errors.firstName = 'First name is required';
-    if (!lastName?.trim()) errors.lastName = 'Last name is required';
-    if (!email || !validator.isEmail(email)) errors.email = 'Valid email is required';
-    if (!password || !validator.isStrongPassword(password)) {
-        errors.password = 'Password must be strong';
-    }
-    
+  if (!firstName?.trim()) errors.firstName = "First name is required";
+  if (!lastName?.trim()) errors.lastName = "Last name is required";
+  if (!email || !validator.isEmail(email))
+    errors.email = "Valid email is required";
+  if (!password || !validator.isStrongPassword(password)) {
+    errors.password = "Password must be strong";
+  }
 
-    if (address) {
-        if (!address.street?.trim()) errors.street = 'Street is required';
-        if (!address.city?.trim()) errors.city = 'City is required';
-    }
+  if (address) {
+    if (!address.street?.trim()) errors.street = "Street is required";
+    if (!address.city?.trim()) errors.city = "City is required";
+  }
+  if (phone && !validator.isMobilePhone(phone, "any"))
+    errors.phone = "Valid phone number is required";
+  if (birthDate && !validator.isDate(birthDate))
+    errors.birthDate = "Valid birth date is required";
 
-    if (Object.keys(errors).length > 0) {
-        return res.status(400).json({ errors });
-    }
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json({ errors });
+  }
 
-    next();
+  next();
 };
 
-
 exports.validateLogin = (req, res, next) => {
-    const { email, password } = req.body;
-    const errors = {};
+  const { email, password } = req.body;
+  const errors = {};
 
-    if (!email || !validator.isEmail(email)) errors.email = 'Valid email is required';
-    if (!password) errors.password = 'Password is required';
+  if (!email || !validator.isEmail(email))
+    errors.email = "Valid email is required";
+  if (!password) errors.password = "Password is required";
 
-    if (Object.keys(errors).length > 0) {
-        return res.status(400).json({ errors });
-    }
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json({ errors });
+  }
 
-    next();
+  next();
 };
